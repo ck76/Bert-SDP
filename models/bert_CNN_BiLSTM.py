cbbca933fc1db1d7f34b24499d1fa4931126d846
程坤酱 <chengkun0804@gmail.com>
2023-01-16 03:11:57 +0900

    注意力

--- models/bert_CNN_BiLSTM.py
+++ models/bert_CNN_BiLSTM.py
@@ -1,4 +1,6 @@
 # coding: UTF-8
+import math
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -108,6 +110,27 @@ class Model(nn.Module):
         print(x.shape)
         return x
 
+    # x: [batch, seq_len, hidden_dim*2]
+    # query : [batch, seq_len, hidden_dim * 2]
+    # 软注意力机制 (key=value=x)
+    def attention_net(self, x, query, mask=None):
+
+        d_k = query.size(-1)  # d_k为query的维度
+
+        # query:[batch, seq_len, hidden_dim*2], x.t:[batch, hidden_dim*2, seq_len]
+        #         print("query: ", query.shape, x.transpose(1, 2).shape)  # torch.Size([128, 38, 128]) torch.Size([128, 128, 38])
+        # 打分机制 scores: [batch, seq_len, seq_len]
+        scores = torch.matmul(query, x.transpose(1, 2)) / math.sqrt(d_k)
+        #         print("score: ", scores.shape)  # torch.Size([128, 38, 38])
+
+        # 对最后一个维度 归一化得分
+        alpha_n = F.softmax(scores, dim=-1)
+        #         print("alpha_n: ", alpha_n.shape)    # torch.Size([128, 38, 38])
+        # 对权重化的x求和
+        # [batch, seq_len, seq_len]·[batch,seq_len, hidden_dim*2] = [batch,seq_len,hidden_dim*2] -> [batch, hidden_dim*2]
+        context = torch.matmul(alpha_n, x).sum(1)
+        return context, alpha_n
+
     def forward(self, x):
         context = x[0]  # 输入的句子
         mask = x[2]  # 对padding部分进行mask，和句子一个size，padding部分用0表示，如：[1, 1, 1, 1, 0, 0]
@@ -143,12 +166,24 @@ class Model(nn.Module):
         print("4")  # torch.Size([1, 1, 768])
         print(out.shape)
         out, (hidden, cell) = self.lstm(out)
-        out = self.dropout(out)
-        print("5")  # torch.Size([1, 1, 1536])
-        print(out.shape)
-        out = self.fc_cnn(out)
-        print("6")
-        print(out.shape)
+
+        # ----------
+        # out = self.dropout(out)
+
+        query = self.dropout(out)
+        # 加入attention机制
+        attn_output, alpha_n = self.attention_net(out, query)
+        print("attn_output")
+        print(attn_output.shape)
+        out = self.fc_cnn(attn_output)
+        # ----------
+
+
+        # print("5")  # torch.Size([1, 1, 1536])
+        # print(out.shape)
+        # out = self.fc_cnn(out)
+        # print("6")
+        # print(out.shape)
         return out
 
 
