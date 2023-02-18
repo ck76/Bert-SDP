901fb075e71bd3c40efcc83bb1ec9c9290fc15f1
ckkkkkkkkkkkk <chengkun@doi.ics.keio.ac.jp>
2023-02-19 01:41:54 +0900

    Update gpu.py

--- gpu.py
+++ gpu.py
@@ -6,3 +6,10 @@ import torch
 print(torch.cuda.is_available())
 print(torch.__version__)
 print(torch.version.cuda)
+
+
+#  pip3 install torch --extra-index-url https://download.pytorch.org/whl/cu117
+#
+# https://pytorch.org/get-started/locally/#windows-pip
+
+# nvidia-smi
